c2b044f9d83d7a5ef339c84fbd24f00ec0a09e15
Antonio Delgado <tonydelro@gmail.com>
2025-06-21 00:48:55 -0600

    Segunda correcion CORS

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,19 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule)
 
   app.enableCors({
     origin: [
-      'http://localhost:5173',
+      'http://localhost:5173', 
       'http://127.0.0.1:5173',
       'https://nexusutd.online',
+      'https://app.nexusutd.online',
     ],
-    credentials: true, // solo si usas cookies o Authorization header
-  });
+    credentials: true, // permite enviar cookies o tokens en headers
+  })
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT ?? 3000)
 }
-bootstrap();
+bootstrap()
