d6953fc0b381ef88be043c11bdc443d90170215b
Antonio Delgado <tonydelro@gmail.com>
2025-06-25 13:33:08 -0600

    Emit websocket event on RFID processing

--- src/inventory/gateways/rfid.gateway.ts
+++ src/inventory/gateways/rfid.gateway.ts
@@ -9,4 +9,12 @@ export class RfidGateway {
   emitTagDetected(tag: string) {
     this.server.emit('rfid-tag-detected', { rfid_tag: tag });
   }
+
+  emitProductUpdated(data: {
+    cardData: any;
+    detailData: any;
+    movementData: any;
+  }) {
+    this.server.to('inventory').emit('product-updated', data);
+  }
 }
